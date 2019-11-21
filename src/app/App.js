eb6cc8a5ab22445953e4e6fc68db06cd42fd4685
tilda21 <tilda21@git.com>
2019-11-21 09:18:30 +0000

    class App with constructor

--- src/app/App.js
+++ src/app/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 'Beautiful',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
         <h1>YODER</h1>
+        <h2>{this.state.test} you are!</h2>
+
         <Switch>
           <Route exact path=""/>
           <Route exact path=""/>
           <Route exact path=""/>
         </Switch>
         
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);;

095acbc93f66045962a66956bacd77caab04a180
Carolina Viero <carolinaviero1@gmail.com>
2019-11-21 08:59:12 +0000

    react router installed

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>YODER</h1>
+        <Switch>
+          <Route exact path=""/>
+          <Route exact path=""/>
+          <Route exact path=""/>
+        </Switch>
         
       </header>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);;

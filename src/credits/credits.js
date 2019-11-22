b73f85e262a3b9183836b2c4a9ce41d4a269ec9c
Edward Watson <e@edwardwatson.com>
2019-11-22 15:16:40 +0000

    minorupdatecredits

--- src/credits/credits.js
+++ src/credits/credits.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect }  from 'react';
-import Crawl from 'react-star-wars-crawl';
-import './credits.css';
+import React, { useState, useEffect } from "react";
+import Crawl from "react-star-wars-crawl";
+import "./credits.css";
 
 // Import the necessary styles, or include them another way with your build process
-import 'react-star-wars-crawl/lib/index.css';
-
-
+import "react-star-wars-crawl/lib/index.css";
 
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
@@ -13,12 +11,9 @@ const useAudio = url => {
 
   const toggle = () => setPlaying(!playing);
 
-  useEffect(
-    () => {
-      playing ? audio.play() : audio.pause();
-    },
-    [playing]
-  );
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [audio, playing]);
 
   return [playing, toggle];
 };
@@ -30,15 +25,16 @@ const Credits = ({ url }) => {
   return (
     <>
       <div id="Margin" className="centerdiv">
-        <button id="Style" className="buttonstyle" onClick={toggle}>{playing ? "🔇" : "🔉"}</button>
+        <button id="Style" className="buttonstyle" onClick={toggle}>
+          {playing ? "🔇" : "🔉"}
+        </button>
       </div>
       <Crawl
         title="Yoder: Intergalactic Dating"
         subTitle="A New Challenge"
-        text="It is a period of intense learning. React error messages, CSS hell, Javascript scope issues. In more ways than one, front-end web frameworks have won many victories against us, technically, socially and emotionally. But we do not despair! After 5 weeks of learning we embarked on a new challenge: Create the best dating app using the Star Wars API in 24 hours.So with love, we present to you - Yoder: Intergalactic Dating, developed in one day only, by yours truly: Matilde Ribeiro, Carolina Viero and Edward Watson.May the force be with you."
+        text="It is a period of intense learning. React error messages, CSS hell, Javascript scope issues. Front-end web frameworks have won victories against us, technically, socially and emotionally. But we do not despair! After 5 weeks of learning we embarked on a new challenge: Create the best dating app using the Star Wars API in 24 hours. So with love, we present to you - Yoder: Intergalactic Dating, developed in one day only by yours truly: Matilde Ribeiro, Carolina Viero and Edward Watson. May the force be with you."
       />
     </>
-    
   );
 };
 
